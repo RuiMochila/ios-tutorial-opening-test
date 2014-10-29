b6b9afaa0bfa68e7a8ef278494554820da6844ef
Rui Mochila <rui.mochila@gmail.com>
2014-10-29 10:02:51 +0000

    Initial Commit

---
+++ TutorialStartTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TutorialStartTest
+//
+//  Created by Rui Mochila on 29/10/14.
+//  Copyright (c) 2014 Rui Mochila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TutorialStartTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TutorialStartTest
+//
+//  Created by Rui Mochila on 29/10/14.
+//  Copyright (c) 2014 Rui Mochila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
